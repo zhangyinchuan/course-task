23cc63ae9acba518c4933ee84c766a7578988ea8
张云川 <114407450+zhangyinchuan@users.noreply.github.com>
2024-12-22 09:50:57 +0800

    Update exp_121.sql

--- exp_121.sql
+++ exp_121.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS `产品表`;
 CREATE TABLE `产品表`  (
-  `产品ID` int NOT NULL,
+  `产品ID` int NOT NULL AUTO_INCREMENT,
   `产品名称` varchar(255) NOT NULL,
   `库存数量` varchar(255) NOT NULL,
   PRIMARY KEY (`产品ID`)
@@ -8,7 +8,7 @@ CREATE TABLE `产品表`  (
 
 DROP TABLE IF EXISTS `订单表`;
 CREATE TABLE `订单表`  (
-  `订单ID` int NOT NULL,
+  `订单ID` int NOT NULL AUTO_INCREMENT,
   `客户ID` int NOT NULL,
   `订单状态` varchar(255) NOT NULL,
   `配送方式` varchar(255) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE `订单表`  (
 
 DROP TABLE IF EXISTS `订单详情表`;
 CREATE TABLE `订单详情表`  (
-  `订单详情ID` int NOT NULL,
+  `订单详情ID` int NOT NULL AUTO_INCREMENT,
   `关联订单ID` int NOT NULL,
   `商品ID` int NOT NULL,
   `购买数量` int NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE `订单详情表`  (
 
 DROP TABLE IF EXISTS `发货记录表`;
 CREATE TABLE `发货记录表`  (
-  `发货ID` int NOT NULL,
+  `发货ID` int NOT NULL AUTO_INCREMENT,
   `关联订单ID` int NOT NULL,
   `发货日期` datetime NOT NULL,
   `发货状态` varchar(255) NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE `发货记录表`  (
 
 DROP TABLE IF EXISTS `客户表`;
 CREATE TABLE `客户表`  (
-  `客户ID` int NOT NULL,
+  `客户ID` int NOT NULL AUTO_INCREMENT,
   `客户名称` varchar(255) NOT NULL,
   `联系方式` varchar(255) NOT NULL,
   PRIMARY KEY (`客户ID`)
@@ -44,7 +44,7 @@ CREATE TABLE `客户表`  (
 
 DROP TABLE IF EXISTS `退货表`;
 CREATE TABLE `退货表`  (
-  `退货ID` int NOT NULL,
+  `退货ID` int NOT NULL AUTO_INCREMENT,
   `关联订单ID` int NOT NULL,
   `退货产品ID` int NOT NULL,
   `退货数量` varchar(255) NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE `退货表`  (
 
 DROP TABLE IF EXISTS `物流状态表`;
 CREATE TABLE `物流状态表`  (
-  `状态ID` int NOT NULL,
+  `状态ID` int NOT NULL AUTO_INCREMENT,
   `关联订单ID` int NOT NULL,
   `当前物流状态` varchar(255) NOT NULL,
   `更新时间` datetime NOT NULL,
@@ -71,7 +71,6 @@ ALTER TABLE `退货表` ADD CONSTRAINT `fk_退货表_订单表_1` FOREIGN KEY (`
 ALTER TABLE `退货表` ADD CONSTRAINT `fk_退货表_产品表_2` FOREIGN KEY (`退货产品ID`) REFERENCES `产品表` (`产品ID`);
 ALTER TABLE `物流状态表` ADD CONSTRAINT `fk_物流状态表_订单表_1` FOREIGN KEY (`关联订单ID`) REFERENCES `订单表` (`订单ID`);
 
--- Assuming there is a notification table to insert records into
 CREATE TABLE IF NOT EXISTS `通知表` (
   `通知ID` int NOT NULL AUTO_INCREMENT,
   `订单ID` int NOT NULL,
@@ -94,7 +93,6 @@ END;
 //
 DELIMITER ;
 
--- Assuming there is a table to store the pickup times
 CREATE TABLE IF NOT EXISTS `取件记录表` (
   `取件ID` int NOT NULL AUTO_INCREMENT,
   `订单ID` int NOT NULL,
@@ -116,7 +114,6 @@ END;
 //
 DELIMITER ;
 
--- Assuming there is a table to store the inventory change logs
 CREATE TABLE IF NOT EXISTS `库存变动日志` (
   `日志ID` int NOT NULL AUTO_INCREMENT,
   `产品ID` int NOT NULL,
@@ -140,7 +137,6 @@ END;
 //
 DELIMITER ;
 
---获取每个客户的总购买金额
 CREATE VIEW `客户总购买金额视图` AS
 SELECT `客户表`.`客户ID`, `客户表`.`客户名称`, SUM(`订单详情表`.`购买数量` * `订单详情表`.`单价`) AS `总购买金额`
 FROM `客户表`
@@ -148,29 +144,25 @@ JOIN `订单表` ON `客户表`.`客户ID` = `订单表`.`客户ID`
 JOIN `订单详情表` ON `订单表`.`订单ID` = `订单详情表`.`关联订单ID`
 GROUP BY `客户表`.`客户ID`, `客户表`.`客户名称`;
 
---获取每个产品的总销售数量和总销售额
 CREATE VIEW `产品总销售视图` AS
 SELECT `产品表`.`产品ID`, `产品表`.`产品名称`, SUM(`订单详情表`.`购买数量`) AS `总销售数量`, SUM(`订单详情表`.`购买数量` * `订单详情表`.`单价`) AS `总销售额`
 FROM `产品表`
 JOIN `订单详情表` ON `产品表`.`产品ID` = `订单详情表`.`商品ID`
 GROUP BY `产品表`.`产品ID`, `产品表`.`产品名称`;
 
- --获取每个订单的详细信息，包括客户信息和发货信息
 CREATE VIEW `订单详细信息视图` AS
-SELECT `订单表`.`订单ID`, `客户表`.`客户名称`, `客户表`.`联系方式`, `订单详情表`.`商品ID`, `订单详情表`.`购买数量`, `订单详情表`.`单价`, `发货记录表`.`发货日期`, `发货记录表`.`发货状态`
+SELECT `订单表`.`订单ID`, `客户表`.`客户名称`, `客户表`.`联系方式`, `订单详情表`.`商品ID`, `订单详情表`.`购买数量`, `订单详情表`.`单价`, `发货记录表`.`发货日期`
 FROM `订单表`
 JOIN `客户表` ON `订单表`.`客户ID` = `客户表`.`客户ID`
 JOIN `订单详情表` ON `订单表`.`订单ID` = `订单详情表`.`关联订单ID`
 LEFT JOIN `发货记录表` ON `订单表`.`订单ID` = `发货记录表`.`关联订单ID`;
 
---获取每个客户的退货记录
 CREATE VIEW `客户退货记录视图` AS
 SELECT `客户表`.`客户ID`, `客户表`.`客户名称`, `退货表`.`退货ID`, `退货表`.`退货产品ID`, `退货表`.`退货数量`, `退货表`.`退货原因`, `退货表`.`退货状态`
 FROM `客户表`
 JOIN `订单表` ON `客户表`.`客户ID` = `订单表`.`客户ID`
 JOIN `退货表` ON `订单表`.`订单ID` = `退货表`.`关联订单ID`;
 
---获取每个订单的物流状态变更历史
 CREATE VIEW `订单物流状态历史视图` AS
 SELECT `订单表`.`订单ID`, `物流状态表`.`状态ID`, `物流状态表`.`当前物流状态`, `物流状态表`.`更新时间`, `物流状态表`.`当前所在地`
 FROM `订单表`
